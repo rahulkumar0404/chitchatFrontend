fce58d67313e070bfb2d5ef6cc66bacfd2785c5a
Rahul@kumar <drkagarwal05@gmail.com>
2024-04-13 23:19:16 +0530

    route and colour update'

--- src/constants/color.js
+++ src/constants/color.js
@@ -13,3 +13,7 @@ export const LIGHT_GRAY_COLOR = '#f5f5f5';
 export const DARK_GRAY_COLOR = '#616161';
 export const WHITE_COLOR = '#fafafa';
 export const BLACK_COLOR = '#000000';
+export const ROYAL_BLUE = '#4B5CC0';
+export const PURPLE_BLUE = '#AF5CC0';
+export const TRANSPARENT_TEAL = '#4BC0C033';
+export const TEAL = '#4BC0C0';
