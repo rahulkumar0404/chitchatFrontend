aee85c9943366cd7daf013b15f830143b8225331
Rahul@kumar <drkagarwal05@gmail.com>
2024-05-22 19:33:01 +0530

    fetch user and route condition modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Suspense, lazy } from 'react';
-import { LayoutLoader } from './components/layouts/Loaders.jsx';
+import { Suspense, lazy, useEffect } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+import { useDispatch, useSelector } from 'react-redux';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import api from './api.js';
 import ProtectRoute from './components/auth/ProtectRoute.jsx';
+import { LayoutLoader } from './components/layouts/Loaders.jsx';
+import { userExists, userNotExists } from './redux/reducers/auth.js';
+import { SocketProvider } from './socket.jsx';
 const Home = lazy(() => import('./pages/Home.jsx'));
 const Login = lazy(() => import('./pages/Login.jsx'));
 const Chat = lazy(() => import('./pages/Chat.jsx'));
@@ -18,41 +23,68 @@ const AdminMessageManagement = lazy(() =>
   import('./pages/admin/MessageManagement.jsx')
 );
 const NotFound = lazy(() => import('./pages/NotFound.jsx'));
-let user = true;
 const App = () => {
+  const dispatch = useDispatch();
+  const { user, isLoading } = useSelector((state) => state.auth);
+  useEffect(() => {
+    async function fetchUser() {
+      try {
+        const data = await api.getProfileUser();
+        dispatch(userExists(data.user));
+      } catch (error) {
+        dispatch(userNotExists());
+        toast.error(error.response.data.message || 'Profile User failed');
+      }
+    }
+    fetchUser()
+  }, [dispatch]);
   return (
     <Router>
-      <Suspense
-        fallback={
-          <div>
-            <LayoutLoader />
-          </div>
-        }
-      >
-        <Routes>
-          <Route element={<ProtectRoute user={user} />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/groups" element={<Groups />} />
-            <Route path="/chat/:chatId" element={<Chat />} />
-          </Route>
-          <Route
-            path="/login"
-            element={
-              <ProtectRoute user={!user} redirect="/">
-                <Login />{' '}
-              </ProtectRoute>
-            }
-          />
+      {isLoading ? (
+        <LayoutLoader />
+      ) : (
+        <Suspense
+          fallback={
+            <div>
+              <LayoutLoader />
+            </div>
+          }
+        >
+          <Routes>
+            <Route
+              element={
+                <SocketProvider>
+                  <ProtectRoute user={user} />
+                </SocketProvider>
+              }
+            >
+              <Route path="/" element={<Home />} />
+              <Route path="/groups" element={<Groups />} />
+              <Route path="/chat/:chatId" element={<Chat />} />
+            </Route>
+            <Route
+              path="/login"
+              element={
+                <ProtectRoute user={!user} redirect="/">
+                  <Login />{' '}
+                </ProtectRoute>
+              }
+            />
 
-          <Route path="/admin" element={<AdminLogin />} />
-          <Route path="/admin/dashboard" element={<AdminDashboard />} />
-          <Route path="/admin/users" element={<AdminUserManagement />} />
-          <Route path="/admin/groups" element={<AdminChatManagement />} />
-          <Route path="/admin/messages" element={<AdminMessageManagement />} />
+            <Route path="/admin" element={<AdminLogin />} />
+            <Route path="/admin/dashboard" element={<AdminDashboard />} />
+            <Route path="/admin/users" element={<AdminUserManagement />} />
+            <Route path="/admin/groups" element={<AdminChatManagement />} />
+            <Route
+              path="/admin/messages"
+              element={<AdminMessageManagement />}
+            />
 
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Suspense>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
+      )}
+      <Toaster position="top-right" />
     </Router>
   );
 };
