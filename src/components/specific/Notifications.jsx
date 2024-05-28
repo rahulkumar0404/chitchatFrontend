1da9fc610f3e6154ed90be5455b412d9d921634e
Rahul@kumar <drkagarwal05@gmail.com>
2024-05-28 10:30:55 +0530

    notification component modified

--- src/components/specific/Notifications.jsx
+++ src/components/specific/Notifications.jsx
@@ -6,38 +6,69 @@ import {
   Button,
   ListItem,
   Avatar,
+  Skeleton,
 } from '@mui/material';
-import { sampleNotifications } from '../../constants/sampleData';
 import { transformImage } from '../../lib/features';
 import { memo } from 'react';
+import {
+  useAcceptFriendRequest,
+  useGetNotificationQuery,
+} from '../../redux/api/api';
+import { useErrors, useMutation } from '../../hooks/hook';
+import { useDispatch, useSelector } from 'react-redux';
+import { setIsNotification } from '../../redux/reducers/others';
+import toast from 'react-hot-toast';
 const Notifications = () => {
-  const friendRequestHandler = ({ id, accept }) => {
-    console.log(id);
-    console.log(accept);
+  const { isNotification } = useSelector((state) => state.others);
+  const dispatch = useDispatch();
+  const { isLoading, data, isError, error } = useGetNotificationQuery();
+  const [acceptRequest] = useAcceptFriendRequest();
+  const friendRequestHandler = async ({ senderId, accept }) => {
+    try {
+      const res = acceptRequest({ senderId, accept });
+      if (res.data) {
+        console.log('Use socket here');
+        console.log(res.data);
+      } else {
+        toast.error(
+          res.data.error.message || 'Failure to accept request Please try again'
+        );
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
   };
+
+  const handleNotification = () => dispatch(setIsNotification(false));
+  useErrors([{ error, isError }]);
   return (
-    <Dialog open>
+    <Dialog open={isNotification} onClose={handleNotification}>
       <Stack p={{ xs: '1rem', sm: '2rem' }}>
         <DialogTitle>Notifications</DialogTitle>
-
-        {sampleNotifications.length > 0 ? (
-          sampleNotifications.map((notification, index) => (
-            <NotificationItem
-              sender={notification.sender}
-              id={notification.id}
-              handler={friendRequestHandler}
-            />
-          ))
+        {isLoading ? (
+          <Skeleton />
         ) : (
-          <Typography textAlign={'center'}>No notifications</Typography>
+          <>
+            {data.result.length > 0 ? (
+              data.result.map((notification, index) => (
+                <NotificationItem
+                  sender={notification.sender}
+                  id={notification._id}
+                  handler={friendRequestHandler}
+                />
+              ))
+            ) : (
+              <Typography textAlign={'center'}>No notifications</Typography>
+            )}
+          </>
         )}
       </Stack>
     </Dialog>
   );
 };
 
-const NotificationItem = memo(({ sender, id }) => {
-  const { name, avatar } = sender;
+const NotificationItem = memo(({ sender, id, handler }) => {
+  const { name, avatar, _id: senderId } = sender;
   return (
     <ListItem>
       <Stack
@@ -62,8 +93,13 @@ const NotificationItem = memo(({ sender, id }) => {
           {`${name} send you a friend Request`}
         </Typography>
         <Stack direction={{ xs: 'column', sm: 'row' }}>
-          <Button onClick={() => handler({ id, accept: true })}>Accept</Button>
-          <Button color="error" onClick={() => handler({ id, accept: false })}>
+          <Button onClick={() => handler({ senderId, accept: true })}>
+            Accept
+          </Button>
+          <Button
+            color="error"
+            onClick={() => handler({ senderId, accept: false })}
+          >
             Reject
           </Button>
         </Stack>
