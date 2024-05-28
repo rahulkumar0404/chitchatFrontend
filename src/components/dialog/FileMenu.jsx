f58a7c0b9b12cc80929744728d3d51af632c06f1
Rahul@kumar <drkagarwal05@gmail.com>
2024-05-28 10:26:13 +0530

    file menu for the dropzone

--- src/components/dialog/FileMenu.jsx
+++ src/components/dialog/FileMenu.jsx
@@ -1,17 +1,73 @@
-import { Menu } from '@mui/icons-material';
-import React from 'react';
-
+import { ListItemText, Menu, MenuItem, MenuList } from '@mui/material';
+import React, { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setIsFileMenu } from '../../redux/reducers/others';
+import {
+  Image as ImageIcon,
+  AudioFile as AudioFileIcon,
+  VideoFile as VideoFileIcon,
+  UploadFile as UploadFileIcon,
+} from '@mui/icons-material';
+import { transformImage } from '../../lib/features';
+import {
+  setAddFile,
+} from '../../redux/reducers/files';
+import { setIsDropzoneOpen } from '../../redux/reducers/others.js';
 const FileMenu = ({ anchorE1 }) => {
+  const dispatch = useDispatch();
+  const { isFileMenu } = useSelector((state) => state.others);
+  const { selectedDataChat } = useSelector((state) => state.files);
+  const [loadedImage, setLoadedImage] = useState([]);
+  const closeFileMenuHandler = (value) => {
+    dispatch(setIsFileMenu(value));
+  };
+  const sendButtonHandler = (fileType) => {
+    dispatch(setAddFile({ fileType: fileType }));
+    dispatch(setIsFileMenu(false));
+    dispatch(setIsDropzoneOpen(true));
+  };
   return (
-    <Menu anchorE1={anchorE1} open={false}>
-      <div style={{ width: '10rem' }}>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque
-        delectus voluptate natus ab quod! Exercitationem, odit. Quod cupiditate
-        aperiam explicabo eligendi placeat nam excepturi illum error ullam
-        distinctio, et omnis.
-      </div>
-    </Menu>
+    <>
+      <Menu
+        anchorEl={anchorE1}
+        open={isFileMenu}
+        onClose={() => closeFileMenuHandler(false)}
+      >
+        <div style={{ width: '10rem' }}>
+          <MenuItem onClick={() => sendButtonHandler('image')}>
+            <ImageIcon />
+            <ListItemText style={{ marginLeft: '0.5rem' }}>Image</ListItemText>
+          </MenuItem>
+          <MenuItem onClick={() => sendButtonHandler('audio')}>
+            <AudioFileIcon />
+            <ListItemText style={{ marginLeft: '0.5rem' }}>Audio</ListItemText>
+          </MenuItem>
+          <MenuItem onClick={() => sendButtonHandler('video')}>
+            <VideoFileIcon />
+            <ListItemText style={{ marginLeft: '0.5rem' }}>Video</ListItemText>
+          </MenuItem>
+          <MenuItem onClick={() => sendButtonHandler('file')}>
+            <UploadFileIcon />
+            <ListItemText style={{ marginLeft: '0.5rem' }}>Files</ListItemText>
+          </MenuItem>
+        </div>
+      </Menu>
+    </>
   );
 };
 
 export default FileMenu;
+
+export const loadedData = () => {
+  const {} = useSelector();
+  return (
+    <>
+      {loadedImage.map((imageFile, index) => {
+        <div>
+          <h2>{imageFile.name}</h2>
+          <img src={transformImage(imageFile.content)} alt={imageFile.name} />
+        </div>;
+      })}
+    </>
+  );
+};
