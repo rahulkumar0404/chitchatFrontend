9c79b9d73a2b96857a456d1537b130d203c9b485
Rahul@kumar <drkagarwal05@gmail.com>
2024-06-07 21:39:42 +0530

    fileslice and chatslice reducer added

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './reducers/auth';
 import { api } from './api/api.js';
 import othersSlice from './reducers/others.js';
-
+import fileSlice from './reducers/files.js';
+import chatSlice from './reducers/chat.js';
 const store = configureStore({
   reducer: {
     [authSlice.name]: authSlice.reducer,
     [othersSlice.name]: othersSlice.reducer,
+    [fileSlice.name]: fileSlice.reducer,
+    [chatSlice.name]: chatSlice.reducer,
     [api.reducerPath]: api.reducer,
   },
   middleware: (getDefaultMiddleware) =>
